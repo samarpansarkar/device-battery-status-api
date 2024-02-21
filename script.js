e8c6da7ca30b041458d26ae3b641f9ac249d8c06
samarpan sarkar <samarpansarkar209@gmail.com>
2024-02-21 15:37:51 +0530

    complete

--- script.js
+++ script.js
@@ -8,41 +8,58 @@ const batteryDisChargingTime = document.querySelector(
 const battery = () => {
   if ("getBattery" in navigator) {
     navigator.getBattery().then((battery) => {
+      console.log(battery);
+
       //Battery  charging change
       battery.addEventListener("chargingchange", () => {
         updateChargingInfo();
       });
       const updateChargingInfo = () => {
         const isCharging = battery.charging ? "Yes" : "No";
-        console.log(isCharging);
+        // console.log(isCharging);
         batteryCharging.innerHTML = isCharging;
       };
 
       //Battery charging time
       battery.addEventListener("chargingtimechange", () => {
-        console.log("Charging time has changed");
+        // console.log("Charging time has changed");
+        updateTimeChange();
       });
+      function updateTimeChange() {
+        batteryChargingTime.innerHTML = battery.chargingTime + " seconds";
+      }
 
       //Battery Discharging time
       battery.addEventListener("dischargingtimechange", () => {
-        console.log("Discharging time has changed");
+        updateDischargingInfo();
       });
+      function updateDischargingInfo() {
+        batteryDisChargingTime.innerHTML = battery.dischargingTime + " seconds";
+        // console.log(
+        //   `Battery discharging time: ${battery.dischargingTime} seconds`
+        // );
+      }
 
       //Battery level change
       battery.addEventListener("levelchange", () => {
         updateLevelChange();
       });
       function updateLevelChange() {
-        const level = battery.level * 100 + "%";
-        batteryLevel.innerHTML = level;
-        console.log(level);
+        const level = battery.level * 100;
+        if (level == 100) {
+          batteryLevel.innerHTML = level + " %  -(fully Charge)";
+        }
+        if (level === 50) {
+          batteryLevel.innerHTML = level + "battery power level is low";
+        }
+        // console.log(level);
       }
-
-      //Battery status
 
       function updateAllBatteryDetails() {
         updateChargingInfo();
         updateLevelChange();
+        updateDischargingInfo();
+        updateTimeChange();
       }
       updateAllBatteryDetails();
     });
